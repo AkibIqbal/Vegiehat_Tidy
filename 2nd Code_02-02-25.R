f45b08186c6001c0143939190432c383a5b83cd3
ashikeelahi3 <ashikeelahi3@gmail.com>
2025-02-03 02:21:24 +0600

    Adding Price

--- 2nd Code_02-02-25.R
+++ 2nd Code_02-02-25.R
@@ -1,27 +1,24 @@
 library(readxl)
 library(tidyverse)
-VEGIEHAT_Pilot_Database <- read_excel("C:/Projects/Vegiehat/VEGIEHAT-Pilot-Database.xlsx")
+VEGIEHAT_Pilot_Database <- read_excel(".\\VEGIEHAT-Pilot-Database.xlsx")
 #View(VEGIEHAT_Pilot_Database)
 
 colnames(VEGIEHAT_Pilot_Database)
 
 Veg_Full_Data = VEGIEHAT_Pilot_Database %>% 
   select(
-    `Submission ID`,UserId, `Submission time`, DistrictName, UpazilaName, `Items to Choose`
+    `Submission ID`, UserId, `Submission time`, DistrictName, UpazilaName, 
+    `Items to Choose`, `Value - Rice`, `Value - Flour`, `Value - Lentil`, 
+    `Value - Soybean Oil`, `Value - Salt`, `Value - Sugar`, `Value - Eggs`, 
+    `Value - Chicken`, `Value - Potato`, `Value - Eggplant`, `Value - Onion`, 
+    `Value - Green Chilli`
   ) %>% 
   separate_rows(
     `Items to Choose`, sep = ","
   ) %>% 
   mutate(
-    `Items to Choose`  = str_trim(
-      `Items to Choose`, side = "both"
-    )
+    `Items to Choose` = str_trim(`Items to Choose`, side = "both")
   ) %>% 
-  distinct(
-    `Submission ID`, `UserId`,`Submission time`, DistrictName, UpazilaName, `Items to Choose`
-  ) %>% 
-  select(`Submission ID`,UserId, `Submission time`, DistrictName, UpazilaName, , `Items to Choose`
-  )%>% 
   mutate(hasProduct = case_when(
     `Items to Choose` == "Rice" ~ 1,
     `Items to Choose` == "Flour" ~ 2,
@@ -36,11 +33,32 @@ Veg_Full_Data = VEGIEHAT_Pilot_Database %>%
     `Items to Choose` == "Onion" ~ 11,
     `Items to Choose` == "Green Chilli" ~ 12,
     TRUE ~ 0
-  ) 
-  )%>%
+  )) %>%
   mutate(
-    Price = NA
-  )
+    Price = case_when(
+      hasProduct == 1 ~ `Value - Rice`,
+      hasProduct == 2 ~ `Value - Flour`,
+      hasProduct == 3 ~ `Value - Lentil`,
+      hasProduct == 4 ~ `Value - Soybean Oil`,
+      hasProduct == 5 ~ `Value - Salt`,
+      hasProduct == 6 ~ `Value - Sugar`,
+      hasProduct == 7 ~ `Value - Eggs`,
+      hasProduct == 8 ~ `Value - Chicken`,
+      hasProduct == 9 ~ `Value - Potato`,
+      hasProduct == 10 ~ `Value - Eggplant`,
+      hasProduct == 11 ~ `Value - Onion`,
+      hasProduct == 12 ~ `Value - Green Chilli`,
+      TRUE ~ NA_real_
+    )
+  ) %>%
+  select(
+    `Submission ID`, UserId, `Submission time`, DistrictName, UpazilaName,
+    `Items to Choose`, hasProduct, Price
+  ) %>%
+  arrange(`Submission ID`, hasProduct) %>% 
+  filter(Price != "NA")
+
 
 View(Veg_Full_Data)
 
+write.csv(Veg_Full_Data, "Veg_Full_Data.csv", row.names = FALSE)
